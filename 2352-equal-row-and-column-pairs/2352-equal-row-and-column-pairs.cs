03569940d5473bc33978dcd3e3a743e57991d762
Nikhil <nikhildeshmukh76@gmail.com>
2026-07-21 22:19:36 +0400

    Time: 26 ms (66.89%), Space: 55.2 MB (100%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int EqualPairs(int[][] grid) {
+        if(grid is null || grid.Length < 1) return 0;
+        int count = 0;
+        int n = grid.Length;
+        Dictionary<string, int> freq = new (n*n);        
+        // iterate through rows
+        for (int row=0; row<n; row++)
+        {
+            var rowKey = string.Join(',',grid[row]);
+            if (!freq.ContainsKey(rowKey))
+                freq.Add(rowKey,1);
+            else
+                freq[rowKey]++;
+        }
+
+        // iterate through columns
+        for (int column=0; column<n; column++)
+        {
+            int[] colElements = new int[n];
+            for (int row=0; row<n; row++)            
+                colElements[row] = grid[row][column];
+            
+            var colKey = string.Join(',',colElements);
+            // If colKey is in dictionary, add its frequency count!
+            if (freq.TryGetValue(colKey, out int rowCount))            
+                count += rowCount;            
+        }
+        return count;
+    }
+}
