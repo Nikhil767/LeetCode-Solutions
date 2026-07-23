b031d183013fe6178061c222783e52c3ef4c2e88
Nikhil <nikhildeshmukh76@gmail.com>
2026-07-23 11:58:10 +0400

    Time: 284 ms (34.48%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT *
 FROM Cinema
-WHERE description != 'boring'
-AND ID%2!=0
+WHERE description <> 'boring'
+AND ID %2 !=0 -- (ID & 1) = 1
 ORDER BY rating DESC
