8c5ab10401d70f9f03735509940b36b9e0baf488
Nikhil <nikhildeshmukh76@gmail.com>
2026-07-29 14:25:21 +0400

    Time: 0 ms (100%), Space: 43.7 MB (49.24%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int[] CountBits(int n) {
+        int[] bits = new int[n+1];
+        bits[0]=0;
+        for (int i =1; i<bits.Length; i++)
+        {
+            bits[i] = bits[i/2] + ((i & 1)==1 ? 1 : 0);
+        }
+        return bits;
+    }
+}
