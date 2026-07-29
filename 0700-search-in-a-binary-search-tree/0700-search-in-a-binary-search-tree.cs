ab2e5580b7cab0b3414c58c95491ca53b286acd2
Nikhil <nikhildeshmukh76@gmail.com>
2026-07-29 13:37:33 +0400

    Time: 0 ms (100%), Space: 51.6 MB (32.16%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cs
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cs
@@ -15,8 +15,7 @@ public class Solution {
     public TreeNode SearchBST(TreeNode root, int val) {
         if (root is null) return null;
         if (val == root.val) return root;
-        if (root.left is not null && val < root.val) return SearchBST(root.left, val);
-        if (root.right is not null && val > root.val) return SearchBST(root.right, val);
-        return null;
+        if (val < root.val) return SearchBST(root.left, val); 
+        return SearchBST(root.right, val);
     }
 }
