6224dee452730a93d048754fa9f9e8f2b1380f2d
Nikhil <nikhildeshmukh76@gmail.com>
2026-07-31 12:02:38 +0400

    Time: 0 ms (100%), Space: 28.9 MB (50.81%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int Tribonacci(int n) {
+        // Handle Base Cases directly
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        int t0 = 0;
+        int t1 = 1;
+        int t2 = 1;
+        for (int i = 3; i <= n; i++)
+        {
+            int next = t0 + t1 + t2;
+            // Shift history window forward
+            t0 = t1;
+            t1 = t2;
+            t2 = next;
+        }
+        return t2;
+    }
+}
