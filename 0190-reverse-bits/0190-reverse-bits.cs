370a227209b5d2d329b62e1b3e1549c1caff30c1
Nikhil <nikhildeshmukh76@gmail.com>
2026-08-01 20:41:08 +0400

    Time: 15 ms (90.13%), Space: 28.9 MB (56.28%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cs
+++ 0190-reverse-bits/0190-reverse-bits.cs
@@ -4,7 +4,7 @@ public class Solution {
         for (int i=0; i<32; i++)
         {
             result <<= 1; // Shift existing bits in result one position to the left.
-            result = result | (n&1); // Extract the last bit of n and append the last bit to result left
+            result |= n&1; // Extract the last bit of n and append the last bit to result left
             n >>>= 1; // Shift n one position to the right using Unsigned Right Shift (>>>)         
         }
         return result;
