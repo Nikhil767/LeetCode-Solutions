00f9066eed5aa9a62e0f7e899870587c43d00fc4
Nikhil <nikhildeshmukh76@gmail.com>
2026-08-13 20:33:30 +0400

    Time: 3 ms (68.49%), Space: 50.5 MB (49.14%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int RomanToInt(string s) {
+        if(string.IsNullOrEmpty(s)) return 0;
+        Dictionary<char,int> data=new();
+        data.Add('I',1);
+        data.Add('V',5);
+        data.Add('X',10);
+        data.Add('L',50);
+        data.Add('C',100);
+        data.Add('D',500);
+        data.Add('M',1000);
+        int result=0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            int currentValue = data[s[i]];
+            int nextValue = (i + 1 < s.Length) ? data[s[i + 1]] : 0;
+
+            if (currentValue < nextValue)
+                result -= currentValue;
+            else
+                result += currentValue;
+        }
+        return result;
+    }
+}
