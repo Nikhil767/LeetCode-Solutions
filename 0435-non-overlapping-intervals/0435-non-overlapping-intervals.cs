6144bfd3f795f40c4f34035cdd5801b0c9746c68
Nikhil <nikhildeshmukh76@gmail.com>
2026-08-15 11:28:17 +0400

    Time: 50 ms (61.09%), Space: 83.6 MB (41.98%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cs
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cs
@@ -2,10 +2,8 @@ public class Solution {
     public int EraseOverlapIntervals(int[][] intervals) {
         // Sort by end time
         Array.Sort(intervals, (a, b) => a[1].CompareTo(b[1]));
-
         int count = 0;
         int lastEnd = intervals[0][1];
-
         for (int i = 1; i < intervals.Length; i++)
         {
             // If overlapping, remove it
