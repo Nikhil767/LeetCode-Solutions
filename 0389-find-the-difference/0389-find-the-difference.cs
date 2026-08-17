ec5d2b5b441394be336e020e89bad586390dd12d
Nikhil <nikhildeshmukh76@gmail.com>
2026-08-17 11:34:39 +0400

    Time: 0 ms (100%), Space: 42.3 MB (77.42%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cs
+++ 0389-find-the-difference/0389-find-the-difference.cs
@@ -1,21 +1,20 @@
 public class Solution {
     public char FindTheDifference(string s, string t) {
         if(string.IsNullOrEmpty(s)) return t[0];
+        const char a='a';
         int[] freq= new int[26];
 
         for (int i=0; i<s.Length; i++)
         {
-            freq[s[i] - 'a']++;
-            freq[t[i] - 'a']--;
+            freq[s[i] - a]++;
+            freq[t[i] - a]--;
         }
-        freq[t[s.Length] - 'a']--;
+        freq[t[s.Length] - a]--;
 
         for (int i = 0; i < freq.Length; i++)
         {
-            if (freq[i] != 0)
-            {
-                return (char)(i + 'a');
-            }
+            if (freq[i] != 0)            
+                return (char)(i + a);            
         }
         return '\0';
     }
