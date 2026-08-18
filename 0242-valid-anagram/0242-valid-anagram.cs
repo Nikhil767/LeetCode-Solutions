5520cfa78c278bfd5f866e8ebca2f64d7a048b00
Nikhil <nikhildeshmukh76@gmail.com>
2026-08-18 17:02:00 +0400

    Time: 0 ms (100%), Space: 43.5 MB (87.1%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        bool result = false;
+        if(string.IsNullOrEmpty(s) || string.IsNullOrEmpty(t)) return result;
+        if(s.Length != t.Length) return result;
+        
+        int[] count= new int[26];
+        
+        for(int i=0; i<s.Length; i++)
+        {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+        
+        for(int j=0; j<count.Length; j++)
+        {
+            if(count[j] != 0)
+                return result;
+        }
+        result=true;
+        return result;        
+    }
+}
