42bf76ab3541ced619dbd27975930b1e1d29cadf
Nikhil <nikhildeshmukh76@gmail.com>
2026-08-19 11:41:16 +0400

    Time: 0 ms (100%), Space: 40.2 MB (65.33%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cs
+++ 0709-to-lower-case/0709-to-lower-case.cs
@@ -1,14 +1,29 @@
 public class Solution {
     public string ToLowerCase(string s) {
         if(string.IsNullOrWhiteSpace(s)) return s;
-        StringBuilder sb = new(s.Length);
-        foreach(char c in s)
+
+        // with String.Create
+        return string.Create(s.Length, s, (span, input)=>
         {
-            if(Char.IsUpper(c))
-                sb.Append((char)(c | 0x20));
-            else
-                sb.Append(c);
-        }
-        return sb.ToString();
+            for(int i=0; i<input.Length; i++)
+            {
+                 if(Char.IsUpper(input[i]))
+                    span[i] =(char)(input[i] | 0x20);
+                else
+                    span[i] =input[i];
+            }
+        });
+
+
+        // // with StringBuilder
+        // StringBuilder sb = new(s.Length);
+        // foreach(char c in s)
+        // {
+        //     if(Char.IsUpper(c))
+        //         sb.Append((char)(c | 0x20));
+        //     else
+        //         sb.Append(c);
+        // }
+        // return sb.ToString();
     }
 }
