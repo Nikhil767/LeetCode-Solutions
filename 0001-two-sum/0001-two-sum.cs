89acba639d1ea1dbad36cf6833589b45a8ac784b
Nikhil <nikhildeshmukh76@gmail.com>
2026-08-21 16:29:32 +0400

    Time: 4 ms (87.61%), Space: 49.3 MB (58.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,13 +1,14 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
         if(nums is null || nums.Length<1) return null;
-        int[] result = new int[2];
+        int[] result = null;
         Dictionary<int, int> freq=new(nums.Length);
         for (int i=0; i<nums.Length; i++)
         {
             var diff = target-nums[i];
             if(freq.TryGetValue(diff, out int index))
             {
+                result = new int[2];
                 result[0]=index;
                 result[1]=i;
                 return result;
