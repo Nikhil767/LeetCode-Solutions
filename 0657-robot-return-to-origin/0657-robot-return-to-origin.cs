5e154b5f795eef28bb3da37396db95afc53ce9c6
Nikhil <nikhildeshmukh76@gmail.com>
2026-08-26 20:57:58 +0400

    Time: 3 ms (100%), Space: 44.6 MB (77.48%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool JudgeCircle(string moves) {
+        int x = 0, y = 0;
+        foreach (char c in moves)
+        {
+            switch (c)
+            {
+                case 'U': y++; break;
+                case 'D': y--; break;
+                case 'L': x--; break;
+                case 'R': x++; break;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+}
