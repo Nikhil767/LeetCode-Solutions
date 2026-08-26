624bc0ad9daa4df94b12df029275c7325a13896c
Nikhil <nikhildeshmukh76@gmail.com>
2026-08-26 20:58:54 +0400

    Time: 3 ms (100%), Space: 44.5 MB (77.48%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cs
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cs
@@ -1,14 +1,18 @@
 public class Solution {
     public bool JudgeCircle(string moves) {
         int x = 0, y = 0;
+        const char U = 'U';
+        const char D = 'D';
+        const char L = 'L';
+        const char R = 'R';
         foreach (char c in moves)
         {
             switch (c)
             {
-                case 'U': y++; break;
-                case 'D': y--; break;
-                case 'L': x--; break;
-                case 'R': x++; break;
+                case U: y++; break;
+                case D: y--; break;
+                case L: x--; break;
+                case R: x++; break;
             }
         }
         return x == 0 && y == 0;
