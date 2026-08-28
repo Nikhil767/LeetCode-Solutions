fb0e82b1db491cd499b969248baf5c772723f2e5
Nikhil <nikhildeshmukh76@gmail.com>
2026-08-28 16:36:14 +0400

    Time: 1 ms (88.78%), Space: 41.6 MB (63.25%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public string AddBinary(string a, string b) {
+        if (string.IsNullOrWhiteSpace(a) || string.IsNullOrWhiteSpace(b)) return "";
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+        int carry = 0;
+        var sb = new StringBuilder(i + j);
+        while (i >= 0 || j >= 0 || carry == 1)
+        {
+            int sum = carry;
+            if (i >= 0)
+            {
+                sum += a[i] - '0';
+                i--;
+            }
+            if (j >= 0)
+            {
+                sum += b[j] - '0';
+                j--;
+            }                
+            sb.Append(sum % 2);
+            carry = sum / 2;
+        }
+        // reverse because we built from right to left
+        // char[] arr = sb.ToString().ToCharArray();
+        // Array.Reverse(arr);
+        // return new string(arr);
+        return String.Create(sb.Length, sb, (span, source)=>
+        {
+            int len = source.Length-1;
+            for (int i=0; i<source.Length; i++)
+            {
+                span[i] = source[len];
+                len--;
+            }
+        });
+    }
+}
