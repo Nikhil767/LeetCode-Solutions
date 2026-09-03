8fe6316ae7bf1f932cef9470354428c2a967e686
Nikhil <nikhildeshmukh76@gmail.com>
2026-09-03 11:48:55 +0400

    Time: 8 ms (78.15%), Space: 44.1 MB (40.74%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public string Multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") 
+            return "0";
+
+        int n1 = num1.Length;
+        int n2 = num2.Length;
+        int[] result = new int[n1 + n2];
+
+        for (int i = n1 - 1; i >= 0; i--)
+        {
+            for (int j = n2 - 1; j >= 0; j--)
+            {
+                int mul = (num1[i] - '0') * (num2[j] - '0');
+                int sum = mul + result[i + j + 1];
+
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
+        }
+
+        // Convert array to string
+        var sb = new StringBuilder();
+        foreach (int digit in result)
+        {
+            if (!(sb.Length == 0 && digit == 0))
+                sb.Append(digit);
+        }
+
+        return sb.ToString();
+    }
+}
