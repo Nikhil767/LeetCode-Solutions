f16f8d47949d43fbdef933c93f923263cc24e9e6
Nikhil <nikhildeshmukh76@gmail.com>
2026-09-05 21:38:18 +0400

    Time: 1 ms (92.27%), Space: 52.6 MB (90.82%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cs
+++ 0002-add-two-numbers/0002-add-two-numbers.cs
@@ -13,13 +13,10 @@ public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
-
         int carry = 0;
-
         while (l1 != null || l2 != null || carry > 0)
         {
             int sum = carry;
-
             if (l1 != null)
             {
                 sum += l1.val;
@@ -33,11 +30,9 @@ public class Solution {
             }
 
             carry = sum / 10;
-
             current.next = new ListNode(sum % 10);
             current = current.next;
         }
-
         return dummy.next;
     }
 }
