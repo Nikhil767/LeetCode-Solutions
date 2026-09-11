1768ffe6e2b5446dd6bfe2ecf1b2bb40f4b6a303
Nikhil <nikhildeshmukh76@gmail.com>
2026-09-11 13:22:55 +0400

    Time: 0 ms (100%), Space: 44.4 MB (87.63%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int DiagonalSum(int[][] mat) {
+        int n = mat.Length;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += mat[i][i];             // primary diagonal
+            sum += mat[i][n - 1 - i];     // secondary diagonal
+        }
+
+        // If n is odd, subtract the center (it was added twice)
+        if (n % 2 == 1)
+            sum -= mat[n / 2][n / 2];
+
+        return sum;
+    }
+}
