6766a8fec201bd0c9fdafba4b9ea8807321f8b84
Nikhil <nikhildeshmukh76@gmail.com>
2026-09-21 10:10:28 +0400

    Time: 2 ms (98.33%), Space: 44.7 MB (73.72%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cs
+++ 0125-valid-palindrome/0125-valid-palindrome.cs
@@ -6,19 +6,19 @@ public class Solution {
         var end = span.Length-1;        
         while(start<= end)
         {
-            bool isFirstLetter = char.IsLetterOrDigit(span[start]);
-            bool isLastLetter = char.IsLetterOrDigit(span[end]);
-            if(!isLastLetter)
+            //bool isFirstLetter = char.IsLetterOrDigit(span[start]);
+            //bool isLastLetter = char.IsLetterOrDigit(span[end]);
+            if(!char.IsLetterOrDigit(span[end]))
             {
                 end--;
                 continue;
             }
-            if(!isFirstLetter)
+            if(!char.IsLetterOrDigit(span[start]))
             {
                 start++;
                 continue;
             }
-            else if((span[start] | 0x20) != (span[end] | 0x20))
+            if((span[start] | 0x20) != (span[end] | 0x20))
             {
                 return false;
             }
