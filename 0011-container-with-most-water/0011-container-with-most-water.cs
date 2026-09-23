ff1ad2b44aee21b291aec43c06f1b755c620e5d8
Nikhil <nikhildeshmukh76@gmail.com>
2026-09-23 13:20:05 +0400

    Time: 1 ms (99.52%), Space: 61.2 MB (93.65%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cs
+++ 0011-container-with-most-water/0011-container-with-most-water.cs
@@ -6,10 +6,10 @@ public class Solution {
         while(start < end)
         {
             var area = (end - start) * Math.Min(height[start], height[end]);
-            if(maxArea <= area)
+            if (maxArea <= area)
                 maxArea = area;
             var shorter = Math.Min(height[start], height[end]);
-            if(height[start] > height[end])            
+            if (height[start] > height[end])            
                 end--;            
             else            
                 start++;            
