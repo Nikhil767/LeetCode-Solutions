200c383d00090842e51018b11a74863b2741e562
Nikhil <nikhildeshmukh76@gmail.com>
2026-09-25 12:13:12 +0400

    Time: 0 ms (100%), Space: 46.6 MB (7.33%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cs
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cs
@@ -13,7 +13,7 @@
  */
 public class Solution {
     public int MaxDepth(TreeNode root) {
-        if (root == null) return 0;
+        if (root is null) return 0;
         int leftHeight = MaxDepth(root.left);
         int rightHeight = MaxDepth(root.right);
         return Math.Max(leftHeight, rightHeight) + 1;
